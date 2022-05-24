9c9739790caf146ec177b1d4f863d3a4f036692b
PACCAUD <anthony.paccaud@capgemini.com>
2022-05-24 17:33:06 +0200

    css header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,9 @@
+/* eslint-disable @next/next/no-img-element */
 import Image from 'next/image';
 import TLogo from '../public/Tonyflix.png';
+import { BellIcon, SearchIcon } from '@heroicons/react/solid';
+import Link from 'next/link';
+import Account from '../public/account3.png';
 
 
 function Header() {
@@ -20,7 +24,20 @@ function Header() {
 					<li className='nav-link'>My List</li>
 				</ul>
 			</div>
-			<div>Header</div>
+			<div className='flex items-center space-x-4 text-base font-medium'>
+				<SearchIcon className='hidden h-6 w-6 sm:inline' />
+				<p className='hidden lg:inline'>Kids</p>
+				<BellIcon className='h-6 w-6' />
+				<div className='space-x-4'>
+					<Link href='/account'>
+						<Image src={Account}
+							alt="Account Logo"
+							width={50}
+							height={50}
+							className='cursor-pointer rounded'/>
+					</Link>
+				</div>
+			</div>
 		</header>
 	);
 }
