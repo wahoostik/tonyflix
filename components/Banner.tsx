bf432d6a62b8ff9a7d5eacf24b35dc87c593a084
PACCAUD <anthony.paccaud@capgemini.com>
2022-05-31 14:20:39 +0200

    CSS Banner

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -19,7 +19,7 @@ function Banner({netflixOriginals}: Props) {
 	const BASE_URL_IMAGE = process.env.NEXT_PUBLIC_API_IMAGE;
 
 	return (
-		<div>
+		<div className='flex flex-col space-y-2 py-16 md:space-y-4 lg:h-[65vh] lg:justify-end lg:pb-12'>
 			<div className="absolute top-0 left-0 -z-10 h-[95vh] w-screen">
 				<Image
 					src={`${BASE_URL_IMAGE}${movie?.backdrop_path || movie?.poster_path}`}
@@ -32,6 +32,7 @@ function Banner({netflixOriginals}: Props) {
 			<h1 className="text-2xl font-bold md:text-4xl lg:text-7xl">
 				{movie?.title || movie?.name || movie?.original_name}
 			</h1>
+			<p className='max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl'>{movie?.overview}</p>
 		</div>
 	);
 }
