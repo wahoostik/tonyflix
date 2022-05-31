2134017f3a5fdedfba2df720a509d6bd75fd518e
PACCAUD <anthony.paccaud@capgemini.com>
2022-05-31 15:09:50 +0200

    button banner + rajout de tailwind text shadow

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import { useEffect, useState } from 'react';
 import { Movie } from '../typing';
+import  { FaPlay } from 'react-icons/fa';
+import { InformationCircleIcon } from '@heroicons/react/solid';
 
 type Props = {
 	netflixOriginals: Movie[]
@@ -33,6 +35,10 @@ function Banner({netflixOriginals}: Props) {
 				{movie?.title || movie?.name || movie?.original_name}
 			</h1>
 			<p className='max-w-xs text-xs md:max-w-lg md:text-lg lg:max-w-2xl lg:text-2xl'>{movie?.overview}</p>
+			<div className='flex space-x-3'>
+				<button className='banner-button bg-white text-black'><FaPlay className='h-4 w-4 text-black md:h-7 md:w-7'/>Lecture</button>
+				<button className='banner-button bg-[gray]/70'>Informations <InformationCircleIcon className='h-5 w-5 md:h-8 md:w-8'/></button>
+			</div>
 		</div>
 	);
 }
