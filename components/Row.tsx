909dc86a1e593a635e50afc599fb2d4efb66b5ee
PACCAUD <anthony.paccaud@capgemini.com>
2022-06-01 09:42:16 +0200

    movies.map

--- components/Row.tsx
+++ components/Row.tsx
@@ -13,8 +13,10 @@ function Row({title, movies}: Props) {
 			<h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl'>{title}</h2>
 			<div className='group relative md:-ml-2'>
 				<ChevronLeftIcon className='chevron-row'/>
-				<div>
-					<Thumbnail />
+				<div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
+					{movies && movies.map((movie) => (
+						<Thumbnail key={movie.id}/>
+					))}
 				</div>
 				<ChevronRightIcon className='chevron-row'/>
 			</div>
--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 function Thumbnail() {
 	return (
 		<div>Thumbnail</div>
