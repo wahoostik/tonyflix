62b5214d276d258cb6032f110f2c244dcc76bcd4
PACCAUD <anthony.paccaud@capgemini.com>
2022-06-01 11:12:22 +0200

    useRef on Row

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,4 +1,5 @@
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline';
+import { useRef, useState } from 'react';
 import { Movie } from '../typing';
 import Thumbnail from './Thumbnail';
 
@@ -8,12 +9,18 @@ type Props = {
 };
 
 function Row({title, movies}: Props) {
+
+	const rowRef = useRef<HTMLDivElement>(null);
+	const [isMoved, setIsMoved] = useState(false);
+
 	return (
 		<div className='h-40 space-y-0.5 md:space-y-2'>
 			<h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl'>{title}</h2>
 			<div className='group relative md:-ml-2'>
 				<ChevronLeftIcon className='chevron-row'/>
-				<div className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2'>
+				<div
+					ref={rowRef}
+					className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2'>
 					{movies && movies.map((movie) => (
 						<Thumbnail key={movie.id} movie={movie}/>
 					))}
