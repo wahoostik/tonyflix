feeb89c968700a6e08370f472f31aeeb01ffd24a
PACCAUD <anthony.paccaud@capgemini.com>
2022-06-01 11:28:44 +0200

    handleClick function

--- components/Row.tsx
+++ components/Row.tsx
@@ -13,11 +13,26 @@ function Row({title, movies}: Props) {
 	const rowRef = useRef<HTMLDivElement>(null);
 	const [isMoved, setIsMoved] = useState(false);
 
+	const handleClick = (direction: string) => {
+		setIsMoved(true);
+
+		if (rowRef.current) {
+			const {scrollLeft, clientWidth} = rowRef.current;
+
+			const scrollTo = direction === 'left' ? scrollLeft - clientWidth : scrollLeft + clientWidth;
+
+			// Défilement fluide pour le scrollbar
+			rowRef.current.scrollTo({left: scrollTo, behavior: 'smooth'});
+		}
+
+	};
+
 	return (
 		<div className='h-40 space-y-0.5 md:space-y-2'>
 			<h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl'>{title}</h2>
 			<div className='group relative md:-ml-2'>
-				<ChevronLeftIcon className='chevron-row'/>
+				<ChevronLeftIcon className='chevron-row'
+				/>
 				<div
 					ref={rowRef}
 					className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2'>
