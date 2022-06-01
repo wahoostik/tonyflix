909130a9dc6775ff91bb29178a544387b95c6fb4
PACCAUD <anthony.paccaud@capgemini.com>
2022-06-01 10:12:32 +0200

    rajout du package tailwind-scrollbar-hide et application sur le component thumbnail

--- components/Row.tsx
+++ components/Row.tsx
@@ -13,7 +13,7 @@ function Row({title, movies}: Props) {
 			<h2 className='w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl'>{title}</h2>
 			<div className='group relative md:-ml-2'>
 				<ChevronLeftIcon className='chevron-row'/>
-				<div className='flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2'>
+				<div className='flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2'>
 					{movies && movies.map((movie) => (
 						<Thumbnail key={movie.id} movie={movie}/>
 					))}
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
 		extend: {},
 	},
 	plugins: [
-		require('tailwindcss-textshadow')
+		require('tailwindcss-textshadow'),
+		require('tailwind-scrollbar-hide')
 	],
 };
