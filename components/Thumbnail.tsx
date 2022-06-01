ed0a34bbb657b63dd18b32eaf3a5ce7dc7dc5969
PACCAUD <anthony.paccaud@capgemini.com>
2022-06-01 10:27:37 +0200

    package scrollbar-tailwind et ajout d'une scrollbar perso sur le body

--- components/Thumbnail.tsx
+++ components/Thumbnail.tsx
@@ -7,13 +7,12 @@ type Props = {
 
 function Thumbnail({movie}: Props) {
 
-	const BASE_URL_IMAGE_THUMBNAIL = process.env.BASE_URL_IMAGE_THUMBNAIL;
-	const BASE_URL = 'https://image.tmdb.org/t/p/w500';
+	const BASE_URL_IMAGE_THUMBNAIL = process.env.NEXT_PUBLIC_API_IMAGE_THUMBNAIL;
 
 	return (
 		<div className='relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105'>
 			<Image
-				src={`${BASE_URL}${movie?.backdrop_path || movie?.poster_path}`}
+				src={`${BASE_URL_IMAGE_THUMBNAIL}${movie?.backdrop_path || movie?.poster_path}`}
 				alt={movie?.title || movie?.name || movie?.original_name}
 				layout='fill'
 				className='rounded-sm object-cover md:rounded'
