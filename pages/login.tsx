9a6fabc19774aa7880006bab7aa61eac5d17c2c5
PACCAUD <anthony.paccaud@capgemini.com>
2022-06-02 12:13:27 +0200

    formulaire login, css formulaire, configuration screens tailwind

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import Head from 'next/head';
 import Image from 'next/image';
 import BackgroundLogin from '../public/netflix-background.jpg';
@@ -18,7 +19,7 @@ function Login() {
 				objectFit='cover'
 				className='-z-10 !hidden opacity-60 sm:!inline'
 			/>
-			<div className='absolute left-4 top-4 md:left-10 md:top-6'>
+			<div className='absolute left-4 top-4 md:left-10 md:top-6 lg:w-72 md:w-72 sm:w-44 vsm:w-44'>
 				<Image
 					src={TLogo}
 					alt="Tonyflix Logo"
@@ -26,6 +27,22 @@ function Login() {
 					height={150}
 					className='cursor-pointer object-contain'/>
 			</div>
+			<form className='relative mt-24 space-y-8 rounded bg-[#010511]/75 py-10 px-6 md:mt-0 md:max-w-md md:px-14'>
+				<h1 className='text-4xl font-bold'>S'identifier</h1>
+				<div className='space-y-4'>
+					<label className='inline-block w-full'>
+						<input type="email" placeholder='E-mail' className='input'/>
+					</label>
+					<label className='inline-block w-full'>
+						<input type="password" placeholder='Mot de passe' className='input'/>
+					</label>
+				</div>
+				<button className='w-full rounded bg-[#e50914] py-3 font-bold'>S'identifier</button>
+				<div className='text-[#9e9e9e] flex'>
+					<h2>Première visite sur Tonyflix ?&ensp;</h2>
+					<button type='submit' className='text-white hover:underline'>Inscrivez-vous.</button>
+				</div>
+			</form>
 		</div>
 	);
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,14 @@ module.exports = {
 					'linear-gradient(to bottom,rgba(20,20,20,0) 0,rgba(20,20,20,.15) 15%,rgba(20,20,20,.35) 29%,rgba(20,20,20,.58) 44%,#141414 68%,#141414 100%);',
 			},
 		},
+		screens: {
+			'vsm': '250px', // => @media (min-width: 250px) { very small screen }
+			'sm': '640px',
+			'md': '768px',
+			'lg': '1024px',
+			'xl': '1280px',
+			'2xl': '1536px',
+		}
 	},
 	plugins: [
 		require('tailwindcss-textshadow'),
