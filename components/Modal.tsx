fa08696a5b80511f3d28d1b43d362ee3e96f9171
PACCAUD <anthony.paccaud@capgemini.com>
2022-06-07 11:24:49 +0200

    css button modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,3 +1,4 @@
+import { XIcon } from '@heroicons/react/outline';
 import MuiModal from '@mui/material/Modal';
 import { useRecoilState } from 'recoil';
 import { modalState } from '../atoms/modalAtom';
@@ -16,7 +17,13 @@ function Modal() {
 				open={showModal}
 				onClose={handleClose}
 			>
-				<h1>Modal</h1>
+				<div>
+					<button
+						onClick={handleClose}
+						className='modalButton absolute right-5 top-5 !z-40 h-9 w-9 border-none bg-[#181818] hover:bg-[#181818]'>
+						<XIcon className='h-6 w-6"'/>
+					</button>
+				</div>
 			</MuiModal>
 		</div>
 	);
